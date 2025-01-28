2f4e5ac232647a847ca6103933575fd0848af711
Jerome Rony <jerome.rony.1@etsmtl.net>
2025-01-27 22:00:39 -0500

    Add tests for surface_euclidean_distance_transform

--- tests/test_distance_transform.py
+++ tests/test_distance_transform.py
@@ -3,6 +3,7 @@ import torch
 
 from distorch import distance_transform
 
+# @formatter:off
 images_sqdistances = (
     ([[0] * 5] * 5, [[float('inf')] * 5] * 5),  # by convention, infinite distance for empty image
     ([[0, 0, 0, 0, 0],
@@ -47,6 +48,7 @@ images_sqdistances = (
        [ 2, 1,  2,  5, 10]]]
      )
 )
+# @formatter:on
 
 devices = ['cpu']
 if torch.cuda.is_available():
@@ -62,4 +64,67 @@ def test_euclidean_distance_transform(image, sqdistances, device_type: str, use_
     image = torch.tensor(image, dtype=torch.bool, device=device)
     distances = torch.tensor(sqdistances, dtype=torch.float, device=device).sqrt()
     edt = distance_transform.euclidean_distance_transform(image)
+    assert torch.allclose(distances, edt), torch.stack((distances, edt, distances - edt), dim=0)
+
+
+# @formatter:off
+surface_images_sqdistances = (
+    ([[0] * 5] * 5, [[float('inf')] * 6] * 6),  # by convention, infinite distance for empty image
+    ([[0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0]],
+     [[8, 5, 4, 4, 5, 8],
+      [5, 2, 1, 1, 2, 5],
+      [4, 1, 0, 0, 1, 4],
+      [4, 1, 0, 0, 1, 4],
+      [5, 2, 1, 1, 2, 5],
+      [8, 5, 4, 4, 5, 8]]),
+    ([[0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0],
+      [0, 0, 1, 1, 0, 0],
+      [0, 1, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0]],
+     [[4, 1, 0, 0, 1, 2, 5],
+      [4, 1, 0, 0, 0, 1, 4],
+      [2, 1, 0, 0, 0, 1, 4],
+      [1, 0, 0, 0, 0, 1, 4],
+      [1, 0, 0, 0, 0, 1, 4],
+      [2, 1, 1, 1, 1, 2, 5]]),
+    ([[[0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0],
+       [0, 0, 0, 1, 0],
+       [0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0]],
+      [[0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0],
+       [0, 1, 0, 0, 0],
+       [0, 0, 0, 0, 0]]],
+     [[[13, 8, 5, 4, 4, 5],
+       [10, 5, 2, 1, 1, 2],
+       [ 9, 4, 1, 0, 0, 1],
+       [ 9, 4, 1, 0, 0, 1],
+       [10, 5, 2, 1, 1, 2],
+       [13, 8, 5, 4, 4, 5]],
+      [[10, 9, 9, 10, 13, 18],
+       [ 5, 4, 4,  5,  8, 13],
+       [ 2, 1, 1,  2,  5, 10],
+       [ 1, 0, 0,  1,  4,  9],
+       [ 1, 0, 0,  1,  4,  9],
+       [ 2, 1, 1,  2,  5, 10]]]
+     )
+)
+# @formatter:on
+
+@pytest.mark.parametrize('device_type', devices)
+@pytest.mark.parametrize('use_pykeops', (False, True))
+@pytest.mark.parametrize('image,sqdistances', surface_images_sqdistances)
+def test_surface_euclidean_distance_transform(image, sqdistances, device_type: str, use_pykeops: bool):
+    distance_transform.use_pykeops = use_pykeops
+    device = torch.device(device_type)
+    image = torch.tensor(image, dtype=torch.bool, device=device)
+    distances = torch.tensor(sqdistances, dtype=torch.float, device=device).sqrt()
+    edt = distance_transform.surface_euclidean_distance_transform(image)
     assert torch.allclose(distances, edt), torch.stack((distances, edt, distances - edt), dim=0)
