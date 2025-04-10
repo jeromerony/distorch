8b3d5f1ddc51f8053f48266560df3a4fe8b73b71
Jerome Rony <jerome.rony.1@etsmtl.net>
2025-04-10 12:37:11 -0400

    Add metrics back

--- distorch/metrics.py
+++ distorch/metrics.py
@@ -53,6 +53,10 @@ def set_metrics(set1: Tensor,
             dist_2_to_1 = torch.cdist(elem_2_not_1, elem_1).amin(dim=1)
 
         metrics['hausdorff'].append(torch.maximum(dist_1_to_2.max(), dist_2_to_1.max()))
+        metrics['average_distance_1_to_2'].append(dist_1_to_2.mean())
+        metrics['average_distance_2_to_1'].append(dist_2_to_1.mean())
+        metrics[f'distance_{quantile:.0%}_1_to_2'].append(torch.quantile(dist_1_to_2, q=quantile))
+        metrics[f'distance_{quantile:.0%}_2_to_1'].append(torch.quantile(dist_2_to_1, q=quantile))
     metrics = {k: torch.stack(v, dim=0) for k, v in metrics.items()}
 
     if ndim == 2:
